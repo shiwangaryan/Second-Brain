99f63f9c60f90918b8170cfdc4bca6414e0958bb
arshiwang <shiwangaryan@gmail.com>
2024-02-21 17:17:26 +0530

    made cahgnes to jorunal page-Flutter

--- lib/features/journal/screens/new_journal.dart
+++ lib/features/journal/screens/new_journal.dart
@@ -1,7 +1,8 @@
+import 'dart:io';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:iconsax/iconsax.dart';
-import 'package:flutter_sound/flutter_sound.dart';
+import 'package:flutter_sound/flutter_sound.dart' as fsound;
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -15,11 +16,15 @@ class NewJournalPage extends StatefulWidget {
 class _NewJournalPageState extends State<NewJournalPage> {
   List<Widget> journalContent = [];
   bool tick = true;
-  FlutterSoundRecorder recorder = FlutterSoundRecorder();
-  FlutterSoundPlayer player = FlutterSoundPlayer();
+  fsound.FlutterSoundRecorder recorder = fsound.FlutterSoundRecorder();
+  fsound.FlutterSoundPlayer player = fsound.FlutterSoundPlayer();
   List<String> filePath = [];
   bool micOn = false;
   bool isPlaying = false;
+  bool isCompleted = false;
+  bool isImageSelected = false;
+  late File? imageFile;
+  ValueNotifier<bool> isPlayingNotifier = ValueNotifier<bool>(false);
 
   @override
   void initState() {
@@ -67,14 +72,15 @@ class _NewJournalPageState extends State<NewJournalPage> {
   }
 
   Future<String?> startRecording() async {
-    final directory = await getApplicationCacheDirectory();
+    final directory = await getApplicationDocumentsDirectory();
     final currentFilePath =
         '${directory.path}/recording_${DateTime.now().millisecondsSinceEpoch}.aac';
     await recorder.openRecorder();
-    await recorder.startRecorder(toFile: currentFilePath, codec: Codec.aacMP4);
+    await recorder.startRecorder(
+        toFile: currentFilePath, codec: fsound.Codec.aacMP4);
 
     setState(() {
-      micOn = !micOn;
+      micOn = true;
       filePath.insert(0, currentFilePath);
     });
     return filePath[0];
@@ -85,30 +91,57 @@ class _NewJournalPageState extends State<NewJournalPage> {
 
     setState(() {
       micOn = false;
+      journalContent.insert(
+        0,
+        AudioItem(
+          filePath: filePath[0],
+          playFunction: startPlaying,
+          stopFunction: pausePlaying,
+          isplayingNotifier: isPlayingNotifier,
+        ),
+        // VoiceNotePlayer(audioLocation: filePath[0]),
+      );
     });
   }
 
-  Future<void> playRecording() async {
-    await player.openPlayer();
-    await player.startPlayer(fromURI: filePath[0]);
-
-    setState(() {
-      isPlaying = true;
-    });
+  Future<void> startPlaying() async {
+    if (!isPlayingNotifier.value) {
+      await player.openPlayer();
+      await player.startPlayer(fromURI: filePath[0]);
+      isPlayingNotifier.value = true;
+    } else {
+      await player.resumePlayer();
+      isPlayingNotifier.value = true;
+      player.onProgress!.listen(
+        (event) async {
+          if (event.duration == event.position) {
+            await player.stopPlayer();
+            isPlayingNotifier.value = false;
+            await player.closePlayer();
+            isCompleted = true;
+          }
+        },
+      );
+    }
   }
 
-  Future<void> stopPlaying() async {
-    await player.stopPlayer();
+  Future<void> pausePlaying() async {
+    if(!isCompleted)
+    {await player.pausePlayer();
+    isPlayingNotifier.value = false;}
 
-    setState(() {
-      isPlaying = false;
-    });
+    // setState(() {
+    //   isPlaying = false;
+    // });
   }
+
+
 
   @override
   void dispose() {
     recorder.closeRecorder();
     player.closePlayer();
+    isPlayingNotifier.dispose();
     super.dispose();
   }
 
@@ -127,46 +160,32 @@ class _NewJournalPageState extends State<NewJournalPage> {
       ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
+        //----------------------
+        //appbar
+        //----------------------
         appBar: CustomAppBar(tick: tick),
+        //----------------------
+        //bottom app bar
+        //----------------------
         bottomNavigationBar: BottomAppBar(
           height: 70,
           color: Colors.white.withOpacity(0.13),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              CustomIconButton(
-                onPressFunc: () => startRecording(),
-                icon: Icons.mic_none,
-                size: 30,
-              ),
+              //---mic & stop button
               micOn
                   ? CustomIconButton(
                       size: 30,
-                      onPressFunc: () {
-                        setState(() {
-                          journalContent.add(
-                            Padding(
-                              padding: const EdgeInsets.only(left: 15.0),
-                              child: CustomIconButton(
-                                onPressFunc: () {
-                                  setState(() {
-                                    isPlaying = !isPlaying;
-                                    isPlaying ? playRecording() : stopPlaying();
-                                  });
-                                },
-                                icon: isPlaying
-                                    ? Icons.pause_circle_outline
-                                    : Icons.play_circle_outline_rounded,
-                                size: 46,
-                              ),
-                            ),
-                          );
-                        });
-                        stopRecording();
-                      },
+                      onPressFunc: () => stopRecording(),
                       icon: Icons.stop_circle_outlined,
                     )
-                  : const SizedBox(width: 0, height: 0),
+                  : CustomIconButton(
+                      onPressFunc: () => startRecording(),
+                      icon: Icons.mic_none,
+                      size: 30,
+                    ),
+              //---camera button
               CustomIconButton(
                 onPressFunc: () {},
                 icon: Icons.photo_camera_outlined,
@@ -175,6 +194,9 @@ class _NewJournalPageState extends State<NewJournalPage> {
             ],
           ),
         ),
+        //----------------------
+        //main body
+        //----------------------
         body: Column(
           children: [
             const SizedBox(height: 20),
@@ -194,16 +216,17 @@ class _NewJournalPageState extends State<NewJournalPage> {
                   color: Colors.white,
                 ),
                 decoration: InputDecoration(
-                    hintText: 'Heading',
-                    hintStyle: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontSize: 28,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.white.withOpacity(0.5),
-                    ),
-                    border: const OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                    )),
+                  hintText: 'Heading',
+                  hintStyle: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 28,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white.withOpacity(0.5),
+                  ),
+                  border: const OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
+                ),
               ),
             ),
             Flexible(
@@ -231,6 +254,63 @@ class _NewJournalPageState extends State<NewJournalPage> {
 //all the custom widgets here
 //
 //
+
+class AudioItem extends StatefulWidget {
+  final String filePath;
+  final VoidCallback playFunction;
+  final VoidCallback stopFunction;
+  final ValueNotifier<bool> isplayingNotifier;
+
+  const AudioItem({
+    required this.filePath,
+    required this.playFunction,
+    required this.stopFunction,
+    required this.isplayingNotifier,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<AudioItem> createState() => _AudioItemState();
+}
+
+class _AudioItemState extends State<AudioItem> {
+  bool playing = false;
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+        valueListenable: widget.isplayingNotifier,
+        builder: (context, isPlaying, child) {
+          return ListTile(
+              leading: playing
+                  ? const Icon(
+                      Icons.pause_circle_outline_rounded,
+                      color: Colors.white,
+                      size: 40,
+                    )
+                  : const Icon(
+                      Icons.play_circle_outline_rounded,
+                      color: Colors.white,
+                      size: 40,
+                    ),
+              title: Text(
+                playing ? 'Stop Playing' : 'Start Playing',
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+              onTap: () {
+                setState(() {
+                  playing = !playing;
+                });
+                playing ? widget.playFunction() : widget.stopFunction();
+                // if (widget.isplayingNotifier.value == false) {
+                //   playing = false;
+                // }
+              });
+        });
+  }
+}
 
 class CustomIconButton extends StatelessWidget {
   const CustomIconButton({
